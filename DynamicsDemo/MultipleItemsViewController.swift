922a848b332392ef086d6cd2c698eebcd49e4f7c
LemonSpike <pranavkasetti@gmail.com>
2016-09-03 18:00:04 +0100

    Latest progress.

--- DynamicsDemo/MultipleItemsViewController.swift
+++ DynamicsDemo/MultipleItemsViewController.swift
@@ -9,8 +9,89 @@
 import UIKit
 
 class MultipleItemsViewController: UIViewController {
-
+    
+    let gravityBehavior = UIGravityBehavior()
+    let collisionBehavior = UICollisionBehavior()
+    let dynamicItemBehavior = UIDynamicItemBehavior()
+    var snapBehavior1: UISnapBehavior!
+    var snapBehavior2: UISnapBehavior!
+    var snapBehavior3: UISnapBehavior!
+    lazy var animator: UIDynamicAnimator = {
+        return UIDynamicAnimator(referenceView: self.view)
+    }()
+    
+    @IBOutlet var boxCollection: [UIView]!
+    
     @IBAction func addViewButtonTapped(sender: AnyObject) {
+        let squareView = createView()
+        view.addSubview(squareView)
+        addDynamicBehaviorsToView(squareView)
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        //        self.view.translatesAutoresizingMaskIntoConstraints = false
+        //        for item in boxCollection {
+        //            item.superview?.translatesAutoresizingMaskIntoConstraints=false
+        //            item.translatesAutoresizingMaskIntoConstraints=false
+        //            self.view.setNeedsLayout()
+        //        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
+        animator.addBehavior(gravityBehavior)
+        
+        collisionBehavior.translatesReferenceBoundsIntoBoundary = true
+        animator.addBehavior(collisionBehavior)
+        
+        dynamicItemBehavior.elasticity = 0.6;
+        animator.addBehavior(dynamicItemBehavior)
+        
+        for item in boxCollection {
+            collisionBehavior.addItem(item)
+        }
+        
+        
+        snapBehavior1=UISnapBehavior(item: boxCollection[0], snapToPoint: boxCollection[0].center)
+        snapBehavior2=UISnapBehavior(item: boxCollection[1], snapToPoint: boxCollection[1].center)
+        snapBehavior3=UISnapBehavior(item: boxCollection[2], snapToPoint: boxCollection[2].center)
+        animator.addBehavior(snapBehavior1)
+        animator.addBehavior(snapBehavior2)
+        animator.addBehavior(snapBehavior3)
+        
+    }
+    
+    func createView() -> UIView {
+        let x = randomNumber(0, max: view.bounds.width)
+        let frame = CGRectMake(CGFloat(x), 0, 40, 40)
+        let squareView = UIView(frame: frame)
+        squareView.backgroundColor = randomColor()
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(MultipleItemsViewController.squareViewTapped(_:)))
+        squareView.addGestureRecognizer(tapGestureRecognizer)
+        return squareView
+    }
+    
+    func randomNumber(min: CGFloat, max: CGFloat) -> CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UInt32.max) * (max - min) + min
+    }
+    
+    func randomColor() -> UIColor {
+        return UIColor(red: randomNumber(0, max: 1), green: randomNumber(0, max: 1), blue: randomNumber(0, max: 1), alpha: 1)
+    }
+    
+    func addDynamicBehaviorsToView(view: UIView) {
+        gravityBehavior.addItem(view)
+        collisionBehavior.addItem(view)
+        dynamicItemBehavior.addItem(view)
+    }
+    
+    func squareViewTapped(gestureRecognizer: UITapGestureRecognizer) {
+        if let view = gestureRecognizer.view {
+            let pushBehavior = UIPushBehavior(items: [view], mode: .Instantaneous)
+            pushBehavior.pushDirection = CGVectorMake(randomNumber(-4, max: 4), randomNumber(-4, max: 4))
+            animator.addBehavior(pushBehavior)
+        }
     }
 }
--- DynamicsDemo/SingleItemViewController.swift
+++ DynamicsDemo/SingleItemViewController.swift
@@ -9,12 +9,29 @@
 import UIKit
 
 class SingleItemViewController: UIViewController {
+    
     let blueSquareView = UIView()
+    var originalBounds = CGRect.zero
+    var originalCenter = CGPoint.zero
+    let ThrowingThreshold: CGFloat = 1000
+    let ThrowingVelocityPadding: CGFloat = 35
+    var attachmentBehavior: UIAttachmentBehavior!
+    var pushBehavior: UIPushBehavior!
+    var itemBehavior: UIDynamicItemBehavior!
+    lazy var animator: UIDynamicAnimator = {
+        return UIDynamicAnimator(referenceView: self.view)
+    }()
+    
+    @IBOutlet weak var redSquare: UIView!
+    @IBOutlet weak var blueSquare: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         addBlueSquareView()
+        animator = UIDynamicAnimator(referenceView: view)
+        originalBounds = blueSquareView.bounds
+        originalCenter = blueSquareView.center
     }
     
     func addBlueSquareView() {
@@ -23,9 +40,95 @@ class SingleItemViewController: UIViewController {
         blueSquareView.backgroundColor = .blueColor()
         view.addSubview(blueSquareView)
     }
-
-    @IBAction func addBehaviorButtonTapped(sender: AnyObject) {
+    
+    @IBAction func handleAttachmentGesture(sender: UIPanGestureRecognizer) {
+        let location = sender.locationInView(self.view)
+        let boxLocation = sender.locationInView(self.blueSquareView)
         
+        
+        switch sender.state {
+        case .Began:
+            print("Your touch start position is \(location)")
+            print("Start location in image is \(boxLocation)")
+            // 1
+            animator.removeAllBehaviors()
+            
+            // 2
+            let centerOffset = UIOffset(horizontal: boxLocation.x - blueSquareView.bounds.midX,
+                                        vertical: boxLocation.y - blueSquareView.bounds.midY)
+            attachmentBehavior = UIAttachmentBehavior(item: blueSquareView,
+                                                      offsetFromCenter: centerOffset, attachedToAnchor: location)
+            
+            // 3
+            redSquare.center = attachmentBehavior.anchorPoint
+            blueSquare.center = location
+            
+            // 4
+            animator.addBehavior(attachmentBehavior)
+            
+        case .Ended:
+            print("Your touch end position is \(location)")
+            print("End location in image is \(boxLocation)")
+            
+            animator.removeAllBehaviors()
+            
+            // 1
+            let velocity = sender.velocityInView(view)
+            let magnitude = sqrt((velocity.x * velocity.x) + (velocity.y * velocity.y))
+            
+            if magnitude > ThrowingThreshold {
+                // 2
+                let pushBehavior = UIPushBehavior(items: [blueSquareView], mode: .Instantaneous)
+                pushBehavior.pushDirection = CGVector(dx: velocity.x / 10, dy: velocity.y / 10)
+                pushBehavior.magnitude = magnitude / ThrowingVelocityPadding
+                
+                animator.addBehavior(pushBehavior)
+                
+                // 3
+                let angle = Int(arc4random_uniform(20)) - 10
+                
+                itemBehavior = UIDynamicItemBehavior(items: [blueSquareView])
+                itemBehavior.friction = 0.2
+                itemBehavior.allowsRotation = true
+                itemBehavior.addAngularVelocity(CGFloat(angle), forItem: blueSquareView)
+                animator.addBehavior(itemBehavior)
+                
+                // 4
+                let timeOffset = Int64(0.4 * Double(NSEC_PER_SEC))
+                dispatch_after(dispatch_time(DISPATCH_TIME_NOW, timeOffset), dispatch_get_main_queue()) {
+                    self.resetDemo()
+                }
+            } else {
+                resetDemo()
+            }
+            
+        default:
+            attachmentBehavior.anchorPoint = sender.locationInView(view)
+            redSquare.center = attachmentBehavior.anchorPoint
+        }
+    }
+    
+    @IBAction func addBehaviorButtonTapped(sender: AnyObject) {
+        let gravityBehavior = UIGravityBehavior(items: [blueSquareView])
+        animator.addBehavior(gravityBehavior)
+        
+        let collisionBehavior = UICollisionBehavior(items: [blueSquareView])
+        collisionBehavior.translatesReferenceBoundsIntoBoundary = true
+        animator.addBehavior(collisionBehavior)
+        
+        let dynamicItemBehavior = UIDynamicItemBehavior(items: [blueSquareView])
+        dynamicItemBehavior.elasticity = 0.6;
+        animator.addBehavior(dynamicItemBehavior)
+    }
+    
+    func resetDemo() {
+        animator.removeAllBehaviors()
+        
+        UIView.animateWithDuration(0.45) {
+            self.blueSquareView.bounds = self.originalBounds
+            self.blueSquareView.center = self.originalCenter
+            self.blueSquareView.transform = CGAffineTransformIdentity
+        }
     }
 }
 
